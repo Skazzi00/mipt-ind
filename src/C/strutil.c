edfda421a566db1e12b565c62e7ef8ea84aeaa62
Alexandr Eremin <eremin.casha@gmail.com>
2020-09-12 23:54:25 +0300

    fix reversed bug and add assert

--- src/C/strutil.c
+++ src/C/strutil.c
@@ -1,7 +1,6 @@
 //
 // Created by Alexandr Eremin on 11.09.2020.
 //
-#include <string.h>
 #include <assert.h>
 #include <ctype.h>
 
@@ -9,6 +8,9 @@
 
 
 static int smartStrViewCmp(const strView *sv1, const strView *sv2, int delta) {
+    assert(sv1);
+    assert(sv2);
+    assert(delta == 1 || delta == -1);
     const char *s1 = sv1->data;
     const char *s2 = sv2->data;
     int i = 0;
@@ -17,23 +19,37 @@ static int smartStrViewCmp(const strView *sv1, const strView *sv2, int delta) {
         i = (int) sv1->length - 1;
         j = (int) sv2->length - 1;
     }
-    while (1) {
+    while (ispunct(s1[i]) || isspace(s1[i])) i += delta;
+    while (ispunct(s2[j]) || isspace(s2[j])) j += delta;
 
+    while (1) {
         while (ispunct(s1[i])) i += delta;
         while (ispunct(s2[j])) j += delta;
-        if (s1[i] != s2[j]) {
-            return s1[i] < s2[j] ? -1 : 1;
-        }
-
-        if (s1[i] == '\0') {
-            return 0;
-        }
-        if (isspace(s1[i])) {
-            while (isspace(s1[i])) i += delta;
-            while (isspace(s2[j])) j += delta;
+        if (delta < 0 && s1[i] < 0 && s2[j] < 0) {
+            const unsigned short c1 =
+                    ((unsigned short) (((unsigned char) s1[i + delta]) << 8u)) | (unsigned char) s1[i];
+            const unsigned short c2 =
+                    ((unsigned short) (((unsigned char) s2[j + delta]) << 8u)) | (unsigned char) s2[j];
+            if (c1 != c2) {
+                return c1 < c2 ? -1 : 1;
+            }
+            i += delta * 2;
+            j += delta * 2;
         } else {
-            i += delta;
-            j += delta;
+            if (s1[i] != s2[j]) {
+                return s1[i] < s2[j] ? -1 : 1;
+            }
+
+            if (s1[i] == '\0') {
+                return 0;
+            }
+            if (isspace(s1[i])) {
+                while (isspace(s1[i])) i += delta;
+                while (isspace(s2[j])) j += delta;
+            } else {
+                i += delta;
+                j += delta;
+            }
         }
     }
 }
