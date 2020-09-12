4471c2db7eea19610d8da8751633096cb591b8ec
Alexandr Eremin <eremin.casha@gmail.com>
2020-09-12 18:06:38 +0300

    improve cpp version

--- src/CPP/sort.cpp
+++ src/CPP/sort.cpp
@@ -9,12 +9,20 @@ int main(int argc, const char **argv) {
         fileInput.open(argv[1]);
     }
     std::istream &in = fileInput.is_open() ? fileInput : std::cin;
-    std::vector<std::string> lines;
-    std::string line;
-    while (getline(in, line)) {
-        lines.push_back(line);
+    std::string data((std::istreambuf_iterator<char>(in)),
+                     std::istreambuf_iterator<char>());
+    std::size_t linesCnt = static_cast<size_t>(std::count(data.begin(), data.end(), '\n') + 1);
+    std::vector<std::string_view> lines(linesCnt);
+    std::size_t curLine = 0;
+    std::size_t cur = 0;
+    for (std::size_t i = 0; i < data.length(); ++i) {
+        if(data[i] == '\n') {
+            lines[curLine++] = std::string_view(data.data() + cur, i - cur);
+            cur = i + 1;
+        }
     }
-    onegin::sort(lines.begin(), lines.end());
+    lines.back() = std::string_view(data.data() + cur, data.length() - cur);
+    std::sort(lines.begin(), lines.end());
     for (const auto &i : lines) {
         std::cout << i << std::endl;
     }
