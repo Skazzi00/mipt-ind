13282f261e916c0855d0d676041acc0ca3c7b1b8
Alexandr Eremin <eremin.casha@gmail.com>
2020-10-07 11:17:28 +0300

    add docs

--- include/C/stack.h
+++ include/C/stack.h
@@ -9,7 +9,11 @@ typedef double StackElem;
 #define STACK_ELEM_FORMAT "%lg"
 
 enum StackError_e {
-    STACK_NULL, STACK_CORRUPT, STACK_INVALID, STACK_OUT_OF_MEMORY, STACK_OK
+    STACK_NULL          = 1,
+    STACK_CORRUPT       = 2,
+    STACK_INVALID       = 3,
+    STACK_OUT_OF_MEMORY = 4,
+    STACK_OK            = 5
 };
 typedef enum StackError_e StackError;
 
@@ -45,29 +49,92 @@ typedef struct {
 
 } Stack;
 
-
+/*!
+ *
+ * @param self pointer to Stack
+ * @return count of elements in stack. In case of error return NPOS
+ */
 size_t StackSize(Stack *self);
 
+/*!
+ *
+ * @param self pointer to Stack
+ * @return Top element of Stack. In case of error return NAN
+ */
 StackElem StackTop(Stack *self);
 
+/*!
+ * Remove top element from Stack.
+ *
+ * @param self pointer to Stack
+ * @return Error code
+ */
 StackError StackPop(Stack *self);
 
+/*!
+ * Push element to Stack.
+ * @param self pointer to Stack
+ * @param value element to push
+ * @return Error code
+ */
 StackError StackPush(Stack *self, StackElem value);
 
+/*!
+ * Resize stack
+ * @param self pointer to Stack
+ * @param size expected size
+ * @return Error code
+ */
 StackError StackResize(Stack *self, size_t size);
 
+/*!
+ * Delete stack created by StackNew
+ * @param self pointer to stack
+ */
 void StackDelete(Stack *self);
 
+/*!
+ * Free stack resources
+ * @param self pointer to stack
+ */
 void StackFree(Stack *self);
 
+/*!
+ * Creates new stack in dynamic memory
+ * @param capacity desired size
+ * @return pointer to Stack
+ */
 Stack *StackNew(size_t capacity);
 
+/*!
+ * Construct stack
+ * @param self pointer to Stack to be constructed
+ * @param capacity desired size
+ * @return Error code
+ */
 StackError StackConstruct(Stack *self, size_t capacity);
 
+
+/*!
+ * Dump stack to logfile
+ * @param self pointer to Stack
+ */
 void StackDump(Stack *self);
 
+/*!
+ * Validate stack
+ * @param self pointer to Stack
+ * @return current error code
+ */
 StackError StackValidate(Stack *self);
 
 #ifdef STACK_HASH_CHECK
+
+/*!
+ *
+ * @param stack pointer to stack
+ * @return hash of Stack
+ */
 StackHashT StackHash(Stack *stack);
+
 #endif
